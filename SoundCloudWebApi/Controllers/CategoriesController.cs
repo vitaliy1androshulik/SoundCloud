c85646773900f688ccafcd2fbeb2d717686bdc78
novakvova <novakvova@gmail>
2025-05-21 16:00:14 +0300

    Simple Validation Model Fluent 12

---
+++ SoundCloudWebApi/Controllers/CategoriesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SoundCloudWebApi.Models.Category;
+
+namespace SoundCloudWebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CategoriesController : ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> Create([FromForm]CategoryCreateModel model)
+    {
+        return Ok();
+    }
+}
