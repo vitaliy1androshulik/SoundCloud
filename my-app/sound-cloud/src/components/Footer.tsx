559d71d8e281c2d19664e23383c9f2e292bac227
vitaliy1androshulik <vatalikandroshulik@gmail.com>
2025-08-10 23:46:12 +0300

    Added the library page and some buttons to it.

---
+++ my-app/sound-cloud/src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+    return (
+        <>
+            <div className={"navigation-text-color text-sm text-center p-3"}>Legal · Privacy · Cookie Policy ·
+                Cookie Manager · Imprint · Artist Resources · Blog ·
+                Charts · Transparency Reports</div>
+            <div className={"text-sm text-center p-4"}>Language: <span className={"text-blue-400"}>English (US)</span></div>
+        </>
+    );
+};
+export default Footer;
