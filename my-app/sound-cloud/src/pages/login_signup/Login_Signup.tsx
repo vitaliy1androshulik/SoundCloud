f3b3e39cd2342e5ee6f70665846155abdcc752bf
novakvova <novakvova@gmail.com>
2025-08-18 14:43:56 +0300

    Working app

--- my-app/sound-cloud/src/pages/login_signup/Login_Signup.tsx
+++ my-app/sound-cloud/src/pages/login_signup/Login_Signup.tsx
@@ -1,7 +1,37 @@
 import React, {useState} from 'react';
 import {useNavigate} from 'react-router-dom';
 import '../../styles/login_signup/Login_Sіgnup.css'
+import type { FormProps } from 'antd';
+import { Button, Checkbox, Form, Input } from 'antd';
+import axios from "axios";
+
+type FieldType = {
+    email?: string;
+    password?: string;
+};
+
 const LoginSignup: React.FC = () => {
+
+    type FieldType = {
+        email?: string;
+        password?: string;
+    };
+    const onFinish: FormProps<FieldType>['onFinish'] = async (values) => {
+        try {
+            const token = await axios.post('http://localhost:5122/api/user/login', values);
+
+            console.log('Success:', token);
+        }
+        catch (error) {
+            console.error(error);
+        }
+
+    };
+
+    const onFinishFailed: FormProps<FieldType>['onFinishFailed'] = (errorInfo) => {
+        console.log('Failed:', errorInfo);
+    };
+
     const navigate = useNavigate();
     const [isChecked, setIsChecked] = useState(false);
 
@@ -18,6 +48,43 @@ const LoginSignup: React.FC = () => {
             <div  className="flex justify-center items-center h-screen bg-color-pink  text-white">
                 <div className="bg-gray-800 p-8 rounded-lg shadow-lg w-80">
                     <h1 className="text-3xl mb-6 text-center">Login</h1>
+
+                    <Form
+                        name="basic"
+                        labelCol={{ span: 8 }}
+                        wrapperCol={{ span: 16 }}
+                        style={{ maxWidth: 600 }}
+                        initialValues={{ remember: true }}
+                        onFinish={onFinish}
+                        onFinishFailed={onFinishFailed}
+                        autoComplete="off"
+                    >
+
+                        <Form.Item<FieldType>
+                            label="Email"
+                            name="email"
+                            rules={[{ required: true, message: 'Please input your username!' }]}
+                        >
+                            <Input />
+                        </Form.Item>
+
+                        <Form.Item<FieldType>
+                            label="Password"
+                            name="password"
+                            rules={[{ required: true, message: 'Please input your password!' }]}
+                        >
+                            <Input.Password />
+                        </Form.Item>
+
+
+
+                        <Form.Item label={null}>
+                            <Button type="primary" htmlType="submit">
+                                Submit
+                            </Button>
+                        </Form.Item>
+
+
 
                     {/* Чекбокс */}
                     <label className="flex items-center space-x-2 mb-6">
@@ -40,6 +107,8 @@ const LoginSignup: React.FC = () => {
                     >
                         Увійти
                     </button>
+
+                    </Form>
                 </div>
             </div>
         </div>
