e7f7f4949791a27298fe1b7826d875ef693f643e
hontarukmykola <gontarykmykola111@gmail.com>
2025-09-11 16:15:35 +0300

    fix admin panel

--- my-app/sound-cloud/src/services/adminApi.ts
+++ my-app/sound-cloud/src/services/adminApi.ts
@@ -1,24 +1,25 @@
 // src/services/adminApi.ts
 import axios from "axios";
+import api from "../utilities/axiosInstance.ts";
 
 const API_URL = import.meta.env.VITE_BASE_URL || "http://localhost:5000/api";
 
 export const adminApi = {
     // Users
-    getUsers: () => axios.get(`${API_URL}/Admin/users`),
-    getUserById: (id: number) => axios.get(`${API_URL}/Admin/users/${id}`),
-    deleteUser: (id: number) => axios.delete(`${API_URL}/Admin/users/${id}`),
+    getUsers: () => api.get(`${API_URL}/Admin/users`),
+    getUserById: (id: number) => api.get(`${API_URL}/Admin/users/${id}`),
+    deleteUser: (id: number) => api.delete(`${API_URL}/Admin/users/${id}`),
     updateUser: (id: number, data: any) =>
-        axios.put(`${API_URL}/Admin/users/${id}`, data),
-    blockUser: (id: number) => axios.patch(`${API_URL}/Admin/users/${id}/block`),
-    unblockUser: (id: number) => axios.patch(`${API_URL}/Admin/users/${id}/unblock`),
+        api.put(`${API_URL}/Admin/users/${id}`, data),
+    blockUser: (id: number) => api.patch(`${API_URL}/Admin/users/${id}/block`),
+    unblockUser: (id: number) => api.patch(`${API_URL}/Admin/users/${id}/unblock`),
 
     // Tracks
-    getTracks: () => axios.get(`${API_URL}/Track`),
-    getTrackById: (id: number) => axios.get(`${API_URL}/Track/${id}`),
-    createTrack: (data: any) => axios.post(`${API_URL}/Track`, data),
-    updateTrack: (id: number, data: any) => axios.put(`${API_URL}/Track/${id}`, data),
-    deleteTrack: (id: number) => axios.delete(`${API_URL}/Track/${id}`),
-    hideTrack: (id: number) => axios.patch(`${API_URL}/Track/${id}/hide`),
-    unhideTrack: (id: number) => axios.patch(`${API_URL}/Track/${id}/unhide`),
+    getTracks: () => api.get(`${API_URL}/Track`),
+    getTrackById: (id: number) => api.get(`${API_URL}/Track/${id}`),
+    createTrack: (data: any) => api.post(`${API_URL}/Track`, data),
+    updateTrack: (id: number, data: any) => api.put(`${API_URL}/Track/${id}`, data),
+    deleteTrack: (id: number) => api.delete(`${API_URL}/Track/${id}`),
+    hideTrack: (id: number) => api.patch(`${API_URL}/Track/${id}/hide`),
+    unhideTrack: (id: number) => api.patch(`${API_URL}/Track/${id}/unhide`),
 };
